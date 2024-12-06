48b82a09929c240b5322fd92d477456258ff3a12
Maynor <maynorx50000@gmail.com>
2024-12-06 08:37:15 -0600

    Add LiveExampleComponent to app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BasicExampleComponent } from './features/basic-example/basic-example.component';
+import { LiveExampleComponent } from './features/live-example/live-example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BasicExampleComponent],
+  imports: [RouterOutlet, BasicExampleComponent, LiveExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
