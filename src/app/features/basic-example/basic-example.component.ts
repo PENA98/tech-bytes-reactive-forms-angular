96f4ad9e0e7369d528dbc2677336f7599b257c81
Maynor <maynorx50000@gmail.com>
2024-12-06 08:21:54 -0600

    Update project configuration and disable CLI analytics
    - Updated the "angular.json" file to include additional configuration for the project.
    - Added a new property "cli" with the value of "analytics" set to false.
    
    Refactor form submission logic in BasicExampleComponent
    
    - Modified the "basic-example.component.html" template to include a submit button and handle form submission.
    - Added a new method "onSubmit()" in the "basic-example.component.ts" file to handle the form submission.
    - Updated the "basic-example.component.ts" file to import the "HttpClient" module and make a POST request to "https://httpbin.org/post" with the form data.
    - Display the response from the POST request in the template.

--- src/app/features/basic-example/basic-example.component.ts
+++ src/app/features/basic-example/basic-example.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import {
   FormArray,
@@ -12,14 +13,18 @@ import {
 @Component({
   selector: 'app-basic-example',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, HttpClientModule],
   templateUrl: './basic-example.component.html',
   styleUrl: './basic-example.component.scss',
 })
 export class BasicExampleComponent {
   form: FormGroup;
+  response: unknown;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+  ) {
     this.form = new FormGroup({
       filters: new FormArray([]),
     });
@@ -42,5 +47,15 @@ export class BasicExampleComponent {
       this.form.get('filters')?.get(index.toString())?.get('name')?.invalid &&
       this.form.get('filters')?.get(index.toString())?.get('name')?.touched
     );
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.httpClient
+      .post('https://httpbin.org/post', this.form.value)
+      .subscribe((response: any) => {
+        this.response = response['data'];
+        console.log(response);
+      });
   }
 }
