3423eeac26bc812b664f5623f5707bb07c75aeb8
Maynor <maynorx50000@gmail.com>
2024-12-06 11:47:36 -0600

    Refactor component imports and update live-example component
    - Refactor app.component.html to import LiveExampleComponent instead of BasicExampleComponent
    - Update live-example.component.html to include a basic reactive form example
    - Update live-example.component.ts to import ReactiveFormsModule and CommonModule

--- src/app/features/live-example/live-example.component.ts
+++ src/app/features/live-example/live-example.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-live-example',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './live-example.component.html',
   styleUrl: './live-example.component.scss',
 })
-export class LiveExampleComponent {}
+export class LiveExampleComponent {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    });
+
+    this.form.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+}
